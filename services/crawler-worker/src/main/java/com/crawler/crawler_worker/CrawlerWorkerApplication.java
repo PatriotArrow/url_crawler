45495a3ee336cac9545ad74ecbbb263a999e462f
PatriotArrow <40357308+PatriotArrow@users.noreply.github.com>
2026-05-12 21:36:50 +0100

    spring boot boilerplate for services

---
+++ services/crawler-worker/src/main/java/com/crawler/crawler_worker/CrawlerWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.crawler.crawler_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerWorkerApplication.class, args);
+	}
+
+}
---
+++ services/crawler-worker/src/test/java/com/crawler/crawler_worker/CrawlerWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawler.crawler_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
