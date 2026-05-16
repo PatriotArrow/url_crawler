5792b65af6af43c3a7a596e3e3f6844f98da19fc
PatriotArrow <40357308+PatriotArrow@users.noreply.github.com>
2026-05-16 04:17:48 +0100

    result entity - jqa

---
+++ services/result-processor/src/main/java/com/crawler/result_processor/model/CrawlResult.java
@@ -0,0 +1,38 @@
+package com.crawler.result_processor.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "crawl_results")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class CrawlResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String jobId;
+
+    @Column
+    private String url;
+
+    @Column
+    private int linksFound;
+
+    @Column
+    private LocalDateTime crawledAt ;
+
+
+
+
+}
